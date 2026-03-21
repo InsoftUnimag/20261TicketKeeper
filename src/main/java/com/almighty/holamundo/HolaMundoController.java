bb3557bdf969eddfa74602f9506bde4dca5102f9
Almighty <133250040+ImZew@users.noreply.github.com>
2026-03-21 06:37:33 -0500

    Update HolaMundoController.java

--- src/main/java/com/almighty/holamundo/HolaMundoController.java
+++ src/main/java/com/almighty/holamundo/HolaMundoController.java
@@ -1,15 +1,16 @@
 package com.almighty.holamundo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HolaMundoController {
 
-    /// Vincula el parámetro de la consulta (query param) con la variable local.
     @GetMapping("/saludo-final")
-    public String saludar(@RequestParam(value = "nombreUsuario") String nombre) {
-        return "Hola mundo " + nombre;
+    public String saludar(@RequestParam(name = "nombreUsuario") String nombre, Model model) {
+        model.addAttribute("usuarioFinal", nombre);
+        return "saludo-final";
     }
 }
